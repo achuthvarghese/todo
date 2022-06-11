d30f2748e5730c36d7333bdd25a681771f1e388c
achuthvarghese <acvknr@gmail.com>
2022-06-11 21:14:43 +0530

    Add Task model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True
+    )
+    title = models.CharField(max_length=60)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
