967045d2b1a4f3a96ef3adabff6e30d9cb7d2ecd
achuthvarghese <acvknr@gmail.com>
2022-06-11 22:01:37 +0530

    Add list of tasks in view

--- base/views.py
+++ base/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+from base.models import Task
+
 
 def index(request):
+    tasks = Task.objects.all()
     context = {
         "title": "Home",
+        "tasks": tasks,
     }
     template = "base/index.html"
     return render(request, template, context)
