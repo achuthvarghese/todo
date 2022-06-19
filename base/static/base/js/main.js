67513a932d641026e6753e0bba2df919d1dc5731
achuthvarghese <acvknr@gmail.com>
2022-06-19 15:24:15 +0530

    Update style

--- base/static/base/js/main.js
+++ base/static/base/js/main.js
@@ -1,1 +1,14 @@
 // Main JS
+
+function markCancel(task_id) {
+    console.log(`Cancel ${task_id}`)
+}
+
+function markDone(task_id) {
+    console.log(`Done ${task_id}`)
+}
+
+function showDetails(task_id) {
+    $('.task-details').hide('slow')
+    $(`#${task_id} .task-details`).toggle('slow')
+}
